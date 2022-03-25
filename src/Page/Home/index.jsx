94bec5702fc0df13d6798a2597c191748b614b19
Felipe <felipe@pop-os.localdomain>
2022-03-25 11:44:15 -0300

    html5 semantico

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -1,24 +1,26 @@
-import homeImg from '../../Assets/Images/undraw_complete_design_re_h75h.svg';
-import { Form } from '../../Components/Form';
+import homeImg from "../../Assets/Images/undraw_complete_design_re_h75h.svg";
+import { Form } from "../../Components/Form";
 
-import './styles.scss';
+import "./styles.scss";
 
 export const Home = () => {
   return (
-    <section>
-      <div className="container-home">
-        <div className="container-text">
-          <h1>
-            <span>Inscreva-se agora</span>
-            <br /> na Newslatter!
-          </h1>
-          <p>Fique por dentro de todas as nossas atualizações.</p>
+    <main>
+      <section>
+        <div className="container-home">
+          <div className="container-text">
+            <h1>
+              <span>Inscreva-se agora</span>
+              <br /> na Newsletter!
+            </h1>
+            <p>Fique por dentro de todas as nossas atualizações.</p>
+          </div>
+          <div className="container-img">
+            <img src={homeImg} alt="figura de uma mulher" />
+          </div>
         </div>
-        <div className="container-img">
-          <img src={homeImg} alt="figura de uma mulher" />
-        </div>
-      </div>
-      <Form />
-    </section>
+        <Form />
+      </section>
+    </main>
   );
 };
